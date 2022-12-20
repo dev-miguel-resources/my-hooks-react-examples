b860fc1afc509036911c81ced6f2148aa869f2a2
dev-miguel-resources <ingenieromiguelch@gmail.com>
2022-12-20 09:18:05 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+import "./index.css";
+
+function App() {
+  const [dependency1, setDependency1] = useState(1);
+  const [dependency2, setDependency2] = useState(1);
+
+  useEffect(() => {
+    console.log("only dependency1", dependency1, dependency2);
+  }, [dependency1]);
+
+  return (
+    <div className="App">
+      <button
+        onClick={() => {
+          setDependency1((prev) => prev + 1);
+        }}
+      >
+        Change dependency1
+      </button>
+      <button
+        onClick={() => {
+          setDependency2((prev) => prev + 1);
+        }}
+      >
+        Change dependency2
+      </button>
+      <button
+        onClick={() => {
+          setDependency1((prev) => prev + 1);
+          setDependency2((prev) => prev + 1);
+        }}
+      >
+        Change both
+      </button>
+    </div>
+  );
+}
+
+export default App;
